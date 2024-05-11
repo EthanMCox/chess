b4884f3e1e818df18ee3382ebb6986d4434e2138
EthanMCox <ethancox@byu.edu>
2024-05-11 12:06:51 -0600

    Decomposed pawnPieceMovesCalculator to remove redundant code

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -5,109 +5,86 @@ import java.util.Collection;
 
 public class PawnMovesCalculator extends PieceMovesCalculator {
 
-  private void addPawnMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor, Collection<ChessMove> moves) {
-    int row = myPosition.getRow();
-    int col = myPosition.getColumn();
-    if (pieceColor == ChessGame.TeamColor.WHITE) {
-      // Add moves for white pawns on the second row two spaces away if no piece is in the way
-      if (row == 2) {
-        ChessPosition frontPosition = new ChessPosition(row + 1, col);
-        if (!hasPiece(board, frontPosition)) {
-          ChessPosition position = new ChessPosition(row + 2, col);
-          if (!hasPiece(board, position)) {
-            moves.add(new ChessMove(myPosition, position, null));
-          }
-        }
-      }
-
-      // Add moves directly in front of white pawn
-      ChessPosition frontPosition = new ChessPosition(row + 1, col);
-      if (!notInBounds(frontPosition)) {
-        if (!hasPiece(board, frontPosition)) {
-          if (row + 1 == 8) {
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.QUEEN));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.ROOK));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.BISHOP));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.KNIGHT));
-          } else {
-            moves.add(new ChessMove(myPosition, frontPosition, null));
-          }
-        }
-      }
-
-      // Add diagonal moves if they exist
-      ChessPosition[] positions = {
-        new ChessPosition(row + 1, col + 1),
-        new ChessPosition(row + 1, col - 1)
-      };
-      for (ChessPosition position : positions) {
-        if (notInBounds(position)) {
-          continue;
-        }
-        if (isOpponentPiece(board, position, pieceColor)) {
-          if (row + 1 == 8) {
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.QUEEN));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.ROOK));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.BISHOP));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.KNIGHT));
-          } else {
-            moves.add(new ChessMove(myPosition, position, null));
-          }
-        }
-      }
-    } else { // Piece Color is black
-      // Add moves for black pawns on the seventh row two spaces away if no piece is in the way
-      if (row == 7) {
-        ChessPosition frontPosition = new ChessPosition(row - 1, col);
-        if (!hasPiece(board, frontPosition)) {
-          ChessPosition position = new ChessPosition(row - 2, col);
-          if (!hasPiece(board, position)) {
-            moves.add(new ChessMove(myPosition, position, null));
-          }
-        }
-      }
-
-      // Add moves directly in front of black pawn
-      ChessPosition frontPosition = new ChessPosition(row - 1, col);
-      if (!notInBounds(frontPosition)) {
-        if (!hasPiece(board, frontPosition)) {
-          if (row - 1 == 1) {
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.QUEEN));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.ROOK));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.BISHOP));
-            moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.KNIGHT));
-          } else {
-            moves.add(new ChessMove(myPosition, frontPosition, null));
-          }
-        }
-      }
-
-      // Add diagonal moves if they exist
-      ChessPosition[] positions = {
-              new ChessPosition(row - 1, col + 1),
-              new ChessPosition(row - 1, col - 1)
-      };
-      for (ChessPosition position : positions) {
-        if (notInBounds(position)) {
-          continue;
-        }
-        if (isOpponentPiece(board, position, pieceColor)) {
-          if (row - 1 == 1) {
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.QUEEN));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.ROOK));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.BISHOP));
-            moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.KNIGHT));
-          } else {
-            moves.add(new ChessMove(myPosition, position, null));
-          }
-        }
-      }
-    }
+  private enum Direction {
+    UP,
+    DOWN
   }
 
   public Collection<ChessMove> pawnPieceMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor) {
     Collection<ChessMove> moves = new ArrayList<>();
     addPawnMoves(board, myPosition, pieceColor, moves);
     return moves;
+  }
+
+  private void addPawnMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor, Collection<ChessMove> moves) {
+    int row = myPosition.getRow();
+    int col = myPosition.getColumn();
+    Direction direction;
+    int startRow;
+    int promotionRow;
+    if (pieceColor == ChessGame.TeamColor.WHITE) {
+      direction = Direction.UP;
+      startRow = 2;
+      promotionRow = 8;
+    } else {
+      direction = Direction.DOWN;
+      startRow = 7;
+      promotionRow = 1;
+    }
+    addPawnDoubleMove(board, myPosition, moves, row, col, startRow, direction);
+    addPawnFrontMove(board, myPosition, moves, row, col, promotionRow, direction);
+    addDiagonalCaptureMoves(board, myPosition, pieceColor, moves, row, col, promotionRow, direction);
+  }
+
+  private void addPawnDoubleMove(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int row, int col, int startRow, Direction direction) {
+    if (row == startRow) {
+      ChessPosition frontPosition = (direction == Direction.UP) ? new ChessPosition(row + 1, col) : new ChessPosition(row - 1, col);
+      if (!hasPiece(board, frontPosition)) {
+        ChessPosition position = (direction == Direction.UP) ? new ChessPosition(row + 2, col) : new ChessPosition(row - 2, col);
+        if (!hasPiece(board, position)) {
+          moves.add(new ChessMove(myPosition, position, null));
+        }
+      }
+    }
+  }
+
+  private void addPawnFrontMove(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves, int row, int col, int promotionRow, Direction direction) {
+    ChessPosition frontPosition = (direction == Direction.UP) ? new ChessPosition(row + 1, col) : new ChessPosition(row - 1, col);
+    if (!notInBounds(frontPosition)) {
+      if (!hasPiece(board, frontPosition)) {
+        int targetRow = (direction == Direction.UP) ? row + 1 : row - 1;
+        if (targetRow == promotionRow) {
+          moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.QUEEN));
+          moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.ROOK));
+          moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.BISHOP));
+          moves.add(new ChessMove(myPosition, frontPosition, ChessPiece.PieceType.KNIGHT));
+        } else {
+          moves.add(new ChessMove(myPosition, frontPosition, null));
+        }
+      }
+    }
+  }
+
+  private void addDiagonalCaptureMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor, Collection<ChessMove> moves, int row, int col, int promotionRow, Direction direction) {
+    int targetRow = (direction == Direction.UP) ? row + 1 : row - 1;
+    ChessPosition[] positions = {
+            new ChessPosition(targetRow, col + 1),
+            new ChessPosition(targetRow, col - 1)
+    };
+    for (ChessPosition position : positions) {
+      if (notInBounds(position)) {
+        continue;
+      }
+      if (isOpponentPiece(board, position, pieceColor)) {
+        if (targetRow == promotionRow) {
+          moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.QUEEN));
+          moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.ROOK));
+          moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.BISHOP));
+          moves.add(new ChessMove(myPosition, position, ChessPiece.PieceType.KNIGHT));
+        } else {
+          moves.add(new ChessMove(myPosition, position, null));
+        }
+      }
+    }
   }
 }
