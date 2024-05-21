1ff9cedb348c3a4762c037b254ee6d3ee373d5e0
EthanMCox <ethancox@byu.edu>
2024-05-21 17:50:38 -0600

    Implemented error handler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import exception.ExceptionResult;
 import spark.*;
+import util.JsonSerializer;
+
+import java.util.Map;
 
 public class Server {
 
@@ -17,7 +21,6 @@ public class Server {
     }
 
     private void createRoutes() {
-//        Spark.get("/hello", (req, res) -> "Hello World"); // Example
       Spark.delete("/db", this::clear);
       Spark.post("/user", this::registerUser);
       Spark.post("session", this::loginUser);
@@ -26,38 +29,43 @@ public class Server {
       Spark.post("/game", this::createGame);
       Spark.post("/game", this::joinGame);
 
-//      Spark.exception(ResponseException.class,this::exceptionHandler);
-      Spark.notFound("<html><body><h1>404 Not Found</h1></body></html>");
+      Spark.exception(ExceptionResult.class,this::exceptionHandler);
+      Spark.notFound("<html><body><h1>404 Error: Not Found</h1></body></html>");
     }
 
-    private Object clear(Request req, Response res) {
+    private Object clear(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object registerUser(Request req, Response res) {
+    private Object registerUser(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object loginUser(Request req, Response res) {
+    private Object loginUser(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object logoutUser(Request req, Response res) {
+    private Object logoutUser(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object listGames(Request req, Response res) {
+    private Object listGames(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object createGame(Request req, Response res) {
+    private Object createGame(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
-    private Object joinGame(Request req, Response res) {
+    private Object joinGame(Request req, Response res) throws ExceptionResult{
         return "Placeholder";
     }
 
+    private void exceptionHandler(ExceptionResult ex, Request req, Response res) {
+        res.status(ex.StatusCode());
+        Map<String, String> errorResponse = Map.of("message", ex.getMessage());
+        res.body(JsonSerializer.serialize(errorResponse));
+    }
 
     public void stop() {
         Spark.stop();
