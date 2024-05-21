1639aca278e3529d1e2b2e4f7d1da0ab51cdbbf4
EthanMCox <ethancox@byu.edu>
2024-05-20 18:50:31 -0600

    Added basic structure for handlers in Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,9 +10,36 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        createRoutes();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private void createRoutes() {
+//        Spark.get("/hello", (req, res) -> "Hello World"); // Example
+        Spark.delete("/db", (req, res) -> { // clear() endpoint
+            return "Placeholder";
+        });
+        Spark.post("/user", (req, res) -> { // register() endpoint
+            return "Placeholder";
+        });
+        Spark.post("session", (req, res) -> { // login() endpoint
+            return "Placeholder";
+        });
+        Spark.delete("session", (req, res) -> { // logout() endpoint
+            return "Placeholder";
+        });
+        Spark.get("/game", (req, res) -> { // listGames() endpoint
+            return "Placeholder";
+        });
+        Spark.post("/game", (req, res) -> { // createGame() endpoint
+            return "Placeholder";
+        });
+        Spark.post("/game", (req, res) -> { // joinGame() endpoint
+            return "Placeholder";
+        });
+        Spark.notFound("<html><body><h1>404 Not Found</h1></body></html>");
     }
 
     public void stop() {
