64a2d852ca57ab39ad739c9afdf29b0cbdc9ae98
EthanMCox <ethancox@byu.edu>
2024-05-21 14:31:35 -0600

    added JsonSerializer class

---
+++ server/src/main/java/util/JsonSerializer.java
@@ -0,0 +1,15 @@
+package util;
+
+import com.google.gson.Gson;
+
+public class JsonSerializer {
+  public static String serialize(Object object) {
+    var serializer = new Gson();
+    return serializer.toJson(object);
+  }
+
+  public static <T> T deserialize(String json, Class<T> type) {
+    var serializer = new Gson();
+    return serializer.fromJson(json, type);
+  }
+}
