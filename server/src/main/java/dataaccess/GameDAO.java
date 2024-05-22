a499ac8ea901c3797c71306a7ef3a6140b7024bd
EthanMCox <ethancox@byu.edu>
2024-05-22 14:24:36 -0600

    Implemented MemoryGameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -7,8 +7,8 @@ import java.util.Collection;
 
 public interface GameDAO {
   void clear() throws ExceptionResult;
-  void createGame(String gameName) throws ExceptionResult;
-  GameData getGame(String gameId) throws ExceptionResult;
+  int createGame(String gameName) throws ExceptionResult;
+  GameData getGame(int gameId) throws ExceptionResult;
   Collection<GameData> listGames() throws ExceptionResult;
   void updateGame(GameData gameData) throws ExceptionResult;
 }
--- server/src/main/java/dataaccess/inMemory/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/inMemory/MemoryGameDAO.java
@@ -1,29 +1,41 @@
 package dataaccess.inMemory;
 
+import chess.ChessGame;
 import dataaccess.GameDAO;
 import model.*;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class MemoryGameDAO implements GameDAO {
+  private int nextId = 1;
+  final private HashMap<Integer, GameData> games = new HashMap<>();
   @Override
   public void clear() {
+    games.clear();
   }
 
   @Override
-  public void createGame(String gameID) {
+  public int createGame(String gameName) {
+    int currentId = nextId;
+    games.put(currentId, new GameData(currentId, null, null, gameName, new ChessGame()));
+    nextId++;
+    return currentId;
   }
 
   @Override
-  public GameData getGame(String gameID) {
-    return null;
+  public GameData getGame(int gameID) {
+    return games.get(gameID);
   }
 
   @Override
   public void updateGame(GameData gameData) {
+    games.put(gameData.gameID(), gameData);
   }
 
   @Override
   public Collection<GameData> listGames() {
-    return null;
+    return new ArrayList<>(games.values());
   }
 }
