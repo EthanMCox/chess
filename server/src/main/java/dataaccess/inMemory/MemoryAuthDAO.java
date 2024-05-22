d1290b223d35654649ee8b161641b80ec9fb8659
EthanMCox <ethancox@byu.edu>
2024-05-22 14:32:14 -0600

    Implemented MemoryAuthDAO

--- server/src/main/java/dataaccess/inMemory/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/inMemory/MemoryAuthDAO.java
@@ -3,27 +3,28 @@ package dataaccess.inMemory;
 import dataaccess.AuthDAO;
 import model.AuthData;
 
+import java.util.HashMap;
+
 public class MemoryAuthDAO implements AuthDAO {
+    final private HashMap<String, AuthData> auths = new HashMap<>();
     @Override
     public void clear() {
+        auths.clear();
     }
 
     @Override
     public AuthData createAuth(String username) {
-        return null;
+        String authToken = generateAuthToken();
+        return new AuthData(authToken, username);
     }
 
     @Override
     public AuthData getAuth(String authToken) {
-        return null;
+        return auths.get(authToken);
     }
 
     @Override
     public void deleteAuth(String authToken) {
-    }
-
-    @Override
-    public String generateAuthToken() {
-        return null;
+        auths.remove(authToken);
     }
 }
