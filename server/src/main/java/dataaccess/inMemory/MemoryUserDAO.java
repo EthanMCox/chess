46daf81a2d25164e577f66e31511f22887c48dee
EthanMCox <ethancox@byu.edu>
2024-05-22 13:55:54 -0600

    Implemented MemoryUserDAO methods and restructured request objects to reduce redundancy

--- server/src/main/java/dataaccess/inMemory/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/inMemory/MemoryUserDAO.java
@@ -1,17 +1,23 @@
 package dataaccess.inMemory;
 
 import model.UserData;
+
+import java.util.HashMap;
+
 public class MemoryUserDAO implements dataaccess.UserDAO {
+    final private HashMap<String, UserData> users = new HashMap<>();
     @Override
     public void clear() {
+        users.clear();
     }
 
     @Override
     public void createUser(String username, String password, String email) {
+        users.put(username, new UserData(username, password, email));
     }
 
     @Override
     public UserData getUser(String username) {
-        return null;
+        return users.get(username);
     }
 }
