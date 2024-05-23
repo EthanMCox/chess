bbe5478a3ecd8c40ec0a98c533216e8fc093a52e
EthanMCox <ethancox@byu.edu>
2024-05-22 22:31:30 -0600

    Got rid of unused methods in model records

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,9 +1,3 @@
 package model;
 public record AuthData(String authToken, String username) {
-  public AuthData updateAuthToken(String authToken) {
-    return new AuthData(authToken, this.username);
-  }
-  public AuthData updateUsername(String username) {
-    return new AuthData(this.authToken, username);
-  }
 }
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,13 +1,4 @@
 package model;
 
 public record UserData(String username, String password, String email) {
-  public UserData updateUsername(String username) {
-    return new UserData(username, this.password, this.email);
-  }
-  public UserData updatePassword(String password) {
-    return new UserData(this.username, password, this.email);
-  }
-  public UserData updateEmail(String email) {
-    return new UserData(this.username, this.password, email);
-  }
 }
