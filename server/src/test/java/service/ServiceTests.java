0363269ef5317ed83511465df5ca06fc4c829b55
EthanMCox <ethancox@byu.edu>
2024-05-24 12:16:52 -0600

    Added registration tests

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -4,9 +4,11 @@ import dataaccess.inmemory.*;
 import exception.ExceptionResult;
 import model.*;
 import dataaccess.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
+import requests.*;
 import results.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,4 +35,55 @@ public class ServiceTests {
     SuccessResult actual = clearService.clearAll();
     assertEquals(expected, actual);
   }
+
+  @Test
+  @DisplayName("Valid registration")
+  void registerValid() throws ExceptionResult {
+    RegisterRequest request = new RegisterRequest("testUser", "1234", "someone@byu.edu");
+    LoginResult expected = new LoginResult("testUser", "authToken");
+    LoginResult actual = userService.register(request);
+    assertEquals(expected.username(), actual.username());
+    assertNotNull(actual.authToken());
+    assertInstanceOf(LoginResult.class, actual);
+  }
+
+  @Test
+  @DisplayName("null password at registration")
+  void registerNullPassword() {
+    RegisterRequest request = new RegisterRequest("testUser", null, "someone@byu.edu");
+    ExceptionResult actual = assertThrows(ExceptionResult.class, () -> userService.register(request));
+    ExceptionResult expected = new ExceptionResult(400, "Error: bad request");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("null username at registration")
+  void registerNullUsername() {
+    RegisterRequest request = new RegisterRequest(null, "1234", "someone@byu.edu");
+    ExceptionResult actual = assertThrows(ExceptionResult.class, () -> userService.register(request));
+    ExceptionResult expected = new ExceptionResult(400, "Error: bad request");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("null email at registration")
+  void registerNullEmail() {
+    RegisterRequest request = new RegisterRequest("testUser", "1234", null);
+    ExceptionResult actual = assertThrows(ExceptionResult.class, () -> userService.register(request));
+    ExceptionResult expected = new ExceptionResult(400, "Error: bad request");
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("Registering a user that already exists")
+  void registerAlreadyExists() throws ExceptionResult {
+    registerValid();
+    RegisterRequest request = new RegisterRequest("testUser", "5678", "someone@byu.edu");
+    ExceptionResult actual = assertThrows(ExceptionResult.class, () -> userService.register(request));
+    ExceptionResult expected = new ExceptionResult(403, "Error: already taken");
+    assertEquals(expected, actual);
+  }
 }
+
+
+
--- shared/src/main/java/exception/ExceptionResult.java
+++ shared/src/main/java/exception/ExceptionResult.java
@@ -1,5 +1,7 @@
 package exception;
 
+import java.util.Objects;
+
 public class ExceptionResult extends Exception {
     final private int statusCode;
 
@@ -10,5 +12,18 @@ public class ExceptionResult extends Exception {
 
     public int statusCode() {
         return statusCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExceptionResult that = (ExceptionResult) o;
+        return statusCode == that.statusCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(statusCode);
     }
 }
