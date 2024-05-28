7c381f15cf46aef78479cf73577ca9c6803c0425
EthanMCox <ethancox@byu.edu>
2024-05-28 14:19:08 -0600

    Added stubs for MySQLGameDAO

--- server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
@@ -1,4 +1,35 @@
 package dataaccess.mysql;
 
-public class MySQLGameDAO {
+import dataaccess.GameDAO;
+import exception.ExceptionResult;
+import model.GameData;
+import model.ListGamesData;
+
+import java.util.Collection;
+
+public class MySQLGameDAO implements GameDAO {
+  @Override
+  public void clear() throws ExceptionResult {
+
+  }
+
+  @Override
+  public int createGame(String gameName) throws ExceptionResult {
+    return 0;
+  }
+
+  @Override
+  public GameData getGame(int gameId) throws ExceptionResult {
+    return null;
+  }
+
+  @Override
+  public Collection<ListGamesData> listGames() throws ExceptionResult {
+    return null;
+  }
+
+  @Override
+  public void updateGame(GameData gameData) throws ExceptionResult {
+
+  }
 }
