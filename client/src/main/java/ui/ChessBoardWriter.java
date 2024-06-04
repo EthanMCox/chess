b341054684b5bedb581bf4260a8536467f693baf
EthanMCox <ethancox@byu.edu>
2024-06-04 15:39:27 -0600

    created ChessBoardWriter class

---
+++ client/src/main/java/ui/ChessBoardWriter.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class ChessBoardWriter {
+
+  private static final int BOARD_SIZE_IN_SQUARES = 8;
+  private static final int SQUARE_SIZE_IN_CHARS = 1;
+  private static final String EMPTY = " ";
+
+
+  public void drawChessBoard() {
+
+  }
+}
