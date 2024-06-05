7e93373febc24bb8434856fe218863fd44a5cddb
EthanMCox <ethancox@byu.edu>
2024-06-05 11:05:52 -0600

    Added basic structure for evaluating user input

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,6 +1,9 @@
 package ui;
 
+import exception.ExceptionResult;
 import server.ServerFacade;
+
+import java.util.Arrays;
 
 public class Client {
   private String visitorName = null;
@@ -13,5 +16,24 @@ public class Client {
     this.server = new ServerFacade();
   }
 
-
+  public String eval(String input) {
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var cmd = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (cmd) {
+        case "register" -> register(params);
+        case "login" -> signIn(params);
+        case "logout" -> signOut();
+        case "creategame" -> createGame(params);
+        case "listgames" -> listGames();
+        case "playgame" -> playGame(params);
+        case "observeGame" -> observeGame(params);
+        case "quit" -> "quit";
+        default -> help();
+      };
+    } catch (ExceptionResult ex) {
+      return ex.getMessage();
+    }
+  }
 }
