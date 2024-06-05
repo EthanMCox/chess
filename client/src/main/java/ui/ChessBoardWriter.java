d4fe60e68342b504c60dc6877114a282472fb34e
EthanMCox <ethancox@byu.edu>
2024-06-04 19:32:48 -0600

    Changed spacing on characters

--- client/src/main/java/ui/ChessBoardWriter.java
+++ client/src/main/java/ui/ChessBoardWriter.java
@@ -10,7 +10,7 @@ import static ui.EscapeSequences.*;
 public class ChessBoardWriter {
 
   private static final int BOARD_SIZE_IN_SQUARES = 8;
-  private static final int SQUARE_SIZE_IN_CHARS = 1;
+  private static final int SQUARE_SIZE_IN_CHARS = 3;
   private static final String EMPTY = " ";
 
   public static void main(String[] args) {
