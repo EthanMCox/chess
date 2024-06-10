6784cffbe85e9216a8495acda74985b9ecf6c7fa
EthanMCox <ethancox@byu.edu>
2024-06-10 14:41:06 -0600

    Implemented LoadGameMessage and Notification

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -1,4 +1,31 @@
 package websocket.messages;
 
-public class LoadGameMessage {
+import chess.ChessGame;
+
+import java.util.Objects;
+
+public class LoadGameMessage extends ServerMessage {
+  private ChessGame game;
+  public LoadGameMessage(ServerMessageType type, ChessGame game) {
+    super(type);
+    this.game = game;
+  }
+
+  public ChessGame getGame() {
+    return this.game;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    LoadGameMessage that = (LoadGameMessage) o;
+    return Objects.equals(game, that.game);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), game);
+  }
 }
