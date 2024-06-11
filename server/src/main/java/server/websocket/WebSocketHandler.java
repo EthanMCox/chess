9dda194deca43fe4ee6616531ed8503829ee6f54
EthanMCox <ethancox@byu.edu>
2024-06-10 22:03:23 -0600

    Implemented stubs for WebSocketHandler functions

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -5,7 +5,7 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import service.WebsocketService;
 import util.JsonSerializer;
-import websocket.commands.UserGameCommand;
+import websocket.commands.*;
 import exception.ExceptionResult;
 
 import java.util.HashMap;
@@ -22,22 +22,38 @@ public class WebSocketHandler {
   }
 
   @OnWebSocketMessage
-  public void onMessage(Session session, String message) {
+  public void onMessage(Session session, String message) { // Message is a JSON string representing a command object
     try {
       UserGameCommand command = JsonSerializer.deserialize(message, UserGameCommand.class);
 
 //      String username = getUsername(command.getAuthString());
       String username = "placeholder";
       throw new ExceptionResult(400, "Not implemented");
-//      switch (command.getCommandType()) {
-//        case CONNECT -> connect(session, username, (ConnectCommand) command);
-//        case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
-//        case LEAVE -> leaveGame(session, username, (LeaveGameCommand) command);
-//        case RESIGN -> resign(session, username, (ResignCommand) command);
-//      }
+      switch (command.getCommandType()) {
+        case CONNECT -> connect(session, username, (ConnectCommand) command);
+        case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
+        case LEAVE -> leaveGame(session, username, (LeaveCommand) command);
+        case RESIGN -> resign(session, username, (ResignCommand) command);
+      }
     } catch (ExceptionResult e) {
       // Potentially refactor this
       e.printStackTrace();
     }
+  }
+
+  private void connect(Session session, String username, ConnectCommand command) {
+    // Stub
+  }
+
+  private void makeMove(Session session, String username, MakeMoveCommand command) {
+    // Stub
+  }
+
+  private void leaveGame(Session session, String username, LeaveCommand command) {
+    // Stub
+  }
+
+  private void resign(Session session, String username, ResignCommand command) {
+    // Stub
   }
 }
