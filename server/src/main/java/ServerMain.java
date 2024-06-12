a60108ee9873cd75efc444349709638a88ed0158
EthanMCox <ethancox@byu.edu>
2024-06-12 14:42:29 -0600

    Removed commented out code in ServerMain

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -10,13 +10,6 @@ public class ServerMain {
             if (args.length >= 1) {
                 port = Integer.parseInt(args[0]);
             }
-//            var userDAO = new MySQLUserDAO();
-//            var gameDAO = new MySQLGameDAO();
-//            var authDAO = new MySQLAuthDAO();
-//            var clearService = new ClearService(userDAO, gameDAO, authDAO);
-//            var gameService = new GameService(gameDAO, authDAO);
-//            var userService = new UserService(userDAO, authDAO);
-//            var server = new Server(userService, gameService, clearService);
             var server = new Server();
             port = server.run(port);
             System.out.printf("Server started on port %d%n", port);
